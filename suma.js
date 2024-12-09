c26c412e4087ce461e5b4032e5d7561a14a02805
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-09 03:51:55 -0400

    Se inserto en la carpeta Experimentos.

--- suma.js
+++
@@ -1,8 +0,0 @@
-
-function Sumar(){
-    var Num1 = parseInt(document.getElementById("Num1").value);
-    var Num2 = parseInt(document.getElementById("Num2").value);
-    let rt = Num1 + Num2;
-    resultado = document.querySelector("#Resultado");
-    return resultado.innerHTML = "Resultado = " + rt
-}
