e6e314fd85a1574463012624348e7929db425934
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2024-12-09 21:43:27 -0400

    Se agregaron las funciones a los botones de sumar y se tmabien se agrego un detalle que te ayuda a  identificar en que pestaña estas.

--- Script.js
+++ Script.js
@@ -13,6 +13,10 @@ function Suma(){
     btn_sumaS.style.borderBottom = "2px solid black"
     let btn_restaR = document.getElementById("btn_restaR");
     btn_restaR.style.borderBottom = "none"
+    let btn_multiM = document.getElementById("btn_multiM");
+    btn_multiM.style.borderBottom = "none"
+    let btn_DivisionD = document.getElementById("btn_DivisionD");
+    btn_DivisionD.style.borderBottom = "none"
     //Textbox de suma
     let Dos_numeros = document.getElementById("Dos_numeros");
     Dos_numeros.style.display = "none";
@@ -63,6 +67,41 @@ function QS_5(){
     let cinco_numeros = document.getElementById("cinco_numeros");
     cinco_numeros.style.display = "Flex";
 }
+//funcion de sumar dos valores//  
+function sumar(){
+    let Num1 = parseFloat(document.getElementById("Num1").value);
+    let Num2 = parseFloat(document.getElementById("Num2").value);
+    let result = Num1 + Num2;
+    resulta = document.querySelector("#Sumar"); 
+    return resulta.innerHTML = result;
+}
+function Sumar1(){
+    let Num1 = parseFloat(document.getElementById("Num11").value);
+    let Num2 = parseFloat(document.getElementById("Num22").value);
+    let Num3 = parseFloat(document.getElementById("Num33").value);
+    let result = Num1 + Num2 + Num3;
+    resulta = document.querySelector("#Sumar1");
+    return resulta.innerHTML = result;  
+}
+function Sumar2(){
+    let Num1 = parseFloat(document.getElementById("Num111").value);
+    let Num2 = parseFloat(document.getElementById("Num222").value);
+    let Num3 = parseFloat(document.getElementById("Num333").value);
+    let Num4 = parseFloat(document.getElementById("Num444").value);
+    let result = Num1 + Num2 + Num3 + Num4;
+    resulta = document.querySelector("#Sumar2");
+    return resulta.innerHTML = result;
+}
+function Sumar3(){
+    let Num1 = parseFloat(document.getElementById("Num1111").value);
+    let Num2 = parseFloat(document.getElementById("Num2222").value);
+    let Num3 = parseFloat(document.getElementById("Num3333").value);
+    let Num4 = parseFloat(document.getElementById("Num4444").value);
+    let Num5 = parseFloat(document.getElementById("Num5555").value);
+    let result = Num1 + Num2 + Num3 + Num4 + Num5;
+    resulta = document.querySelector("#Sumar3");
+    return resulta.innerHTML = result;
+}
 //Funcion de resta
 function Resta(){
     let Resta = document.getElementById("Resta");
@@ -78,6 +117,10 @@ function Resta(){
     btn_sumaS.style.borderBottom = "none"
     let btn_restaR = document.getElementById("btn_restaR");
     btn_restaR.style.borderBottom = "2px solid black"
+    let btn_multiM = document.getElementById("btn_multiM");
+    btn_multiM.style.borderBottom = "none"
+    let btn_DivisionD = document.getElementById("btn_DivisionD");
+    btn_DivisionD.style.borderBottom = "none"
     //Textbox de resta
     let Dos_numerosR = document.getElementById("Dos_numerosR");
     Dos_numerosR.style.display = "none";
@@ -143,6 +186,10 @@ function Multiplicacion(){
     btn_sumaS.style.borderBottom = "none"
     let btn_restaR = document.getElementById("btn_restaR");
     btn_restaR.style.borderBottom = "none"
+    let btn_multiM = document.getElementById("btn_multiM");
+    btn_multiM.style.borderBottom = "2px solid black"
+    let btn_DivisionD = document.getElementById("btn_DivisionD");
+    btn_DivisionD.style.borderBottom = "none"
     //Textbox de Multiplicacion
     let Dos_numerosR = document.getElementById("Dos_numerosM");
     Dos_numerosR.style.display = "none";
@@ -209,6 +256,10 @@ function Division(){
     btn_sumaS.style.borderBottom = "none"
     let btn_restaR = document.getElementById("btn_restaR");
     btn_restaR.style.borderBottom = "none"
+    let btn_multiM = document.getElementById("btn_multiM");
+    btn_multiM.style.borderBottom = "none"
+    let btn_DivisionD = document.getElementById("btn_DivisionD");
+    btn_DivisionD.style.borderBottom = "2px solid black"
 }
 function QD_2(){
     let Dos_numerosD = document.getElementById("Dos_numerosD");
